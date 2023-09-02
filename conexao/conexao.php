f98c6aa3f4483435fa1995f43fb1f6b3662dbcbe
Thaynison <cthaynison@outlook.com.br>
2023-09-01 22:42:12 -0300

    Update conexao.php

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST=$_ENV["DB_HOST"];
-$DB_USER=$_ENV["DB_USER"];
-$DB_PASSWORD=$_ENV["DB_PASSWORD"];
-$DB_NAME=$_ENV["DB_NAME"];
-$DB_PORT=$_ENV["DB_PORT"];
+$DB_HOST="containers-us-west-113.railway.app";
+$DB_NAME="railway";
+$DB_PASSWORD="mmbcUTHymnuEUj2cuPa5";
+$DB_PORT="7609";
+$DB_USER="root";
 $conexao = mysqli_connect("DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "DB_PORT") or die ('Não foi possivel conectar ao banco de dados');
 ?>
