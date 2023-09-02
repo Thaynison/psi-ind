56b3ed8989bdec234a7c778b80b3dc3ee9cb040a
Thaynison <cthaynison@outlook.com.br>
2023-09-01 22:28:10 -0300

    att

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,8 +1,9 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'Thaynison');
-define('SENHA', 'noFoge');
-define('DB', 'planejamento');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar ao banco de dados');
+session_start();
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$conexao = mysqli_connect("DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "DB_PORT") or die ('Não foi possivel conectar ao banco de dados');
 ?>
