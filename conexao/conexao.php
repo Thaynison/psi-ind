61b95d7f615dcdcc272d185378dece3a47d3f3c0
Thaynison <cthaynison@outlook.com.br>
2023-09-01 22:43:31 -0300

    Update conexao.php

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -4,5 +4,5 @@ $DB_NAME="railway";
 $DB_PASSWORD="mmbcUTHymnuEUj2cuPa5";
 $DB_PORT="7609";
 $DB_USER="root";
-$conexao = mysqli_connect("DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "DB_PORT") or die ('Não foi possivel conectar ao banco de dados');
+$conexao = mysqli_connect($DB_HOST, $DB_NAME, $DB_PASSWORD, $DB_PORT, $DB_USER) or die ('Não foi possivel conectar ao banco de dados');
 ?>
